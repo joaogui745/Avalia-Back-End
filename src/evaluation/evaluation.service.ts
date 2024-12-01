d7997f86bee9844be4d3308d614780ef854fe77b
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2024-12-01 01:23:05 -0300

    :100: Correção do DTO, Tratamento de erros do usuário completo, tratamento de erros do Evaluation, todas rotas implementadas testadas

--- src/evaluation/evaluation.service.ts
+++ src/evaluation/evaluation.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable , NotFoundException } from '@nestjs/common';
 import { CreateEvaluationDto } from './dto/createEvaluation.dto';
 import { UpdateEvaluationDto } from './dto/updateEvaluation.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,15 +7,72 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class EvaluationService {
   constructor(private readonly prisma : PrismaService){}
   async create(EvaluationData: CreateEvaluationDto) {
-    return await this.prisma.evaluation.create({data : EvaluationData});
+    const { content, userID, professorID, subjectID } = EvaluationData;
+    const [ userExists, professorExistis, subjectExists] = await Promise.all([
+      this.prisma.user.findUnique({ where: { id: userID } }),
+      this.prisma.professor.findUnique({ where: { id: professorID } }),
+      this.prisma.subject.findUnique({ where: { id: subjectID} }),
+    ]);
+    if (!userExists){
+      throw new NotFoundException(`User with ID ${userID} not found`); 
+    }
+    if (!professorExistis){
+      throw new NotFoundException(`Professor with ID ${professorID} not found`);
+    }
+    if (!subjectExists){
+      throw new NotFoundException(`Subject with ID ${subjectID} not found`); //todas rotas funcionando :joia:
+    }
+    return this.prisma.evaluation.create({
+      data: { content, user:{connect:{ id : userID }}, professor:{connect: { id: professorID }}, subject:{connect:{ id: subjectID }},
+        
+      }
+    });
   }
+  
 
   async findAll() {
-    return await this.prisma.evaluation.findMany();
+    return await this.prisma.evaluation.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            department: true,
+            course: true,
+            profilePic: true,
+          }
+        },
+        professor: true, 
+        subject: true,
+      }, //retorna outras informações junto
+    });
   }
 
   async findOne(evaluationID: number) {
-    return await this.prisma.evaluation.findUnique({where: {id : evaluationID}});
+
+    const evaluation = await this.prisma.evaluation.findUnique({
+      where: { id: evaluationID },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            department: true,
+            course: true,
+            profilePic: true,
+          }
+        },
+        professor: true,
+        subject: true, //retorna outras informações junto
+      },
+    });
+    if (!evaluation){
+      throw new NotFoundException(`Evaluation with ID ${evaluationID} not found`); //rota funcionando
+
+    }
+    return evaluation
   }
 
   async update(evaluationID: number, evaluationData: UpdateEvaluationDto) {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -22,23 +22,60 @@ export class UserService {
         course: true,
         profilePic: true,
         createdAt: true,
-        updatedAt: true,
+        updatedAt: true, //retorna as informções, menos as sensíveis (senha)
         },
     });
         return user; //novo return, sem retornar senha do usuário
     }
     async findUser(userId : number){
-        return await this.prisma.user.findUnique({
-            where : {id: userId},
-        })
+
+        const user = await this.prisma.user.findUnique({where : {id: userId}});
+        if (!user){
+            throw new NotFoundException(`User with ID ${userId} not found.`);
+        }
+
+        const usuario = await this.prisma.user.findUnique({
+            where: {id: userId}, select:{
+                id: true,
+                name: true,
+                email :true,
+                department: true,
+                course: true,
+                profilePic: true,
+                createdAt: true,
+                updatedAt: true
+                
+            },
+        });
+            return usuario //retorna sem senha
+
     }
     async deleteUser(userId : number){
-        return this.prisma.user.delete({where : {id : userId}});
+        const user = await this.prisma.user.findUnique({where : {id: userId}});
+        if (!user){
+            throw new NotFoundException(`User with ID ${userId} not found.`); //rota funcionando
+        }
+        const usuario = await this.prisma.user.delete({
+            where: {id: userId}, select:{
+                id: true,
+                name: true,
+                email :true,
+                department: true,
+                course: true,
+                profilePic: true,
+                createdAt: true,
+                updatedAt: true
+                
+            },
+        });
+            return usuario //retorna sem senha
+
+       
     }
     async updateUser(userId : number, userData : UpdateUserDto){
         const user = await this.prisma.user.findUnique({where : {id: userId}});
         if (!user){
-            throw new NotFoundException('User with ID ${userID} not found.');
+            throw new NotFoundException(`User with ID ${userId} not found.`);
         }
 
         if (userData.email){
@@ -61,12 +98,17 @@ export class UserService {
                 course: true,
                 profilePic: true,
                 createdAt: true,
-                updatedAt: true,
+                updatedAt: true, //retorna as informções, menos as sensíveis (senha)
 
             },
         });
     }
     async findByEmail(email: string) { //Buscar por email
+        // const user = await this.prisma.user.findUnique({where : {email: email}});
+        // if (!user){
+        //     throw new NotFoundException(`User with email address ${email} not found.`);
+        // }
+        // o findbyemail é apenas um método para verificar se já existe um email, ele não precisa retornar se não encontrou
         return await this.prisma.user.findUnique({
 
             where: {email: email},
