868d3bcde1ff84f9ca95177d0937e1aee65ac40e
Caio Mercon <cmpc29@hotmail.com.br>
2024-12-17 17:15:05 -0300

    :sparkles:feat: Torna as rotas publicas em subject

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -2,31 +2,37 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, Pars
 import { SubjectService } from './subject.service';
 import { CreateSubjectDto } from './dto/createSubject.dto';
 import { UpdateSubjectDto } from './dto/updateSubject.dto';
+import { Public } from 'src/auth/decorators/isPublic.decorator';
 
 @Controller('subject')
 export class SubjectController {
   constructor(private readonly subjectService: SubjectService) {}
 
+  @Public()
   @Post()
   create(@Body(ValidationPipe) subjectData: CreateSubjectDto) {
     return this.subjectService.create(subjectData);
   }
 
+  @Public()
   @Get()
   findAll() {
     return this.subjectService.findAll();
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.subjectService.findOne(id);
   }
 
+  @Public()
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) subjectData: UpdateSubjectDto) {
     return this.subjectService.update(id, subjectData);
   }
 
+  @Public()
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.subjectService.remove(id);
