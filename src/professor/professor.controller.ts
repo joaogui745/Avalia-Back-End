f2cedd38628095234a1c96b90ae71948668487dd
Caio Mercon <cmpc29@hotmail.com.br>
2024-12-17 17:14:32 -0300

    :sparkles:feat: Torna as rotas publicas em professor

--- src/professor/professor.controller.ts
+++ src/professor/professor.controller.ts
@@ -2,31 +2,37 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, Pars
 import { ProfessorService } from './professor.service';
 import { CreateProfessorDto } from './dto/createProfessor.dto';
 import { UpdateProfessorDto } from './dto/updateProfessor.dto';
+import { Public } from 'src/auth/decorators/isPublic.decorator';
 
 @Controller('professor')
 export class ProfessorController {
   constructor(private readonly professorService: ProfessorService) {}
 
+  @Public()
   @Post()
   create(@Body(ValidationPipe) professorData: CreateProfessorDto) {
     return this.professorService.create(professorData);
   }
 
+  @Public()
   @Get()
   findAll() {
     return this.professorService.findAll();
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.professorService.findOne(id);
   }
 
+  @Public()
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateProfessorDto: UpdateProfessorDto) {
     return this.professorService.update(+id, updateProfessorDto);
   }
 
+  @Public()
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.professorService.remove(id);
