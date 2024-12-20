7c5cc44d9f3115388e71942cbee4812c9a5d4124
Caio Mercon <cmpc29@hotmail.com.br>
2024-12-20 00:06:36 -0300

    :wrench: fix: Habilita o CORS no projeto

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common'; // ValidantionPipe injetado
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe()) //Validation injetado
   await app.listen(process.env.PORT ?? 3333);
 }
